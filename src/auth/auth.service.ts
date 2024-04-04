af826489c3f0651ffc2cd8cab3dec9fc0f34c51a
cronero95 <49892098+cronero95@users.noreply.github.com>
2024-04-03 21:45:54 -0500

    Encrypt password and don't send it in response

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -3,6 +3,8 @@ import { Injectable, BadRequestException, InternalServerErrorException } from '@
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 
+import * as bcryptjs from "bcryptjs";
+
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateAuthDto } from './dto/update-auth.dto';
 
@@ -18,17 +20,27 @@ export class AuthService {
 
    async create(createUserDto: CreateUserDto): Promise<User> {
     try{
-      const newUser = new this.userModel(createUserDto);
-      return  await newUser.save();
+      const {password, ...userData} = createUserDto;
+
+      const newUser = new this.userModel({
+        password: bcryptjs.hashSync(password, 10),
+        ...userData
+      });
+
+      await newUser.save();
+      const {password: _, ...user} = newUser.toJSON();
+
+      return user;
+
     } catch(error) {
+
       if(error.code === 11000) {
         throw new BadRequestException(`The email ${createUserDto.email} has already been used`)
       } else {
         throw new InternalServerErrorException('Something terrible has happened')
       }
+      
     }
-    
-    
   }
 
   findAll() {
